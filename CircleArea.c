acebb3f904c852e86f631a2ad6b8d60ed2fe7698
Reman Biswas <67569511+biswasreman@users.noreply.github.com>
2023-01-08 21:17:42 +0530

    Create CircleArea.c

---
+++ CircleArea.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    float radius;
+
+    printf("Enter the given radius: ");
+    scanf("%f", &radius);
+
+
+    printf("The area of circle is %f", 3.14 * radius * radius);
+
+    return 0;
+}
