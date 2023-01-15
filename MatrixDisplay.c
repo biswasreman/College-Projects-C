eb6037bd29cbd9aafe85db33df40d1f8f6535d2a
Reman Biswas <67569511+biswasreman@users.noreply.github.com>
2023-01-15 20:50:59 +0530

    Add files via upload

---
+++ MatrixDisplay.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+
+    int arr[2][3], i, j;
+
+    printf("Enter number in matrix : ");
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+            scanf("%d", &arr[i][j]);
+    }
+
+    printf("Display in matrix form : ");
+    printf("\n");
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+            printf("%4d", arr[i][j]);
+            printf("\n");
+    }
+}
+
+
+
+
